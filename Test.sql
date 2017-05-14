5d45afa6dd7ab44e570ea5d97087bde22e0329d4
Veres84 <igorveres34@gmail.com>
2017-05-14 23:47:18 +0300

    Add user

--- Test.sql
+++ Test.sql
@@ -1,12 +1,28 @@
-drop database if exists Test;
-create database Test;
-use Test;
+drop database if exists test;
+create database test;
+use test;
 
 create table Employees (
-		ID int,
+		ID int primary key auto_increment,
         Name varchar(30),
         Birthday date,
         Email varchar(30),
-        Position varchar(30),
         Departmen varchar(30)
+);
+
+insert into Employees(
+		Name,
+        Birthday,
+        Email,
+        Departmen
+) values (
+		"Mukola",
+        null,
+        "xxx@gmail.com",
+        "IT"
+),(
+		"Mariya",
+        null,
+        "mariya@gmail.com",
+        "bos"
 )
